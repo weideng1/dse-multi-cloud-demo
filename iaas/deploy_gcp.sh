b0871a1c57e9390739be07130cebfd2ffa5a7652
phact <estevezsebastian@gmail.com>
2018-10-25 20:47:23 -0400

    params

--- iaas/deploy_gcp.sh
+++ iaas/deploy_gcp.sh
@@ -23,7 +23,7 @@ while getopts 'hd:p:' opt; do
     ;;
     d) deploy="$OPTARG"
     ;;
-    d) parameters="$params"
+    d) parameters="$OPTARG"
     ;;
     \?) echo "Invalid option -$OPTARG" >&2
         exit 1
@@ -35,5 +35,5 @@ echo "Deploying 'clusterParameters.yaml' in GCP gcloud deployment: $deploy"
 if [ -z $parameters ]; then
     gcloud deployment-manager deployments create $deploy --config ./gcp/clusterParameters.yaml --labels delpoyer-app=assethub,create_user=sebastian_estevez_datastax_com,org=presales
 else
-    gcloud deployment-manager deployments create $deploy --config ./gcp/clusterParameters.yaml --properties ${params}
+    gcloud deployment-manager deployments create $deploy --config ./gcp/clusterParameters.yaml --properties ${parameters}
 fi
