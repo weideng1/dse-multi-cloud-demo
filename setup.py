e48fa2ca1c82d51c5a087b704c0366c157d9c101
phact <estevezsebastian@gmail.com>
2019-04-18 23:09:42 -0400

    use openjdk rather than hotspot

--- setup.py
+++ setup.py
@@ -52,8 +52,7 @@ download_token = os.environ.get('academy_token').strip()
 bashCommand = 'ssh -o StrictHostKeyChecking=no -i '+ ssh_key+ ' '+ username+'@'+server_ip+' \'sudo apt-get install -y python software-properties-common; \
 sudo apt-add-repository -y ppa:webupd8team/java; \
 sudo apt-get update; \
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections; \
-sudo apt-get install -y oracle-java8-installer; \
+sudo apt-get install -y openjdk-8-jdk; \
 echo "deb https://'+repo_user+':'+download_token+'@debian.datastax.com/enterprise \
 stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.sources.list; \
 curl -L https://debian.datastax.com/debian/repo_key | sudo apt-key add - ; \
